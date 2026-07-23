0036a569ec8205dbaa200fe5d2b5b491c554f75d
abeythomas1997 <abeythomas990@gmail.com>
2026-07-23 13:17:49 +1000

    cleaned

---
+++ src/processing/__init__.py
@@ -0,0 +1,11 @@
+from src.processing.clean_aemo import (
+    clean_operational_demand,
+    find_missing_intervals,
+    validate_operational_demand,
+)
+
+__all__ = [
+    "clean_operational_demand",
+    "find_missing_intervals",
+    "validate_operational_demand",
+]
